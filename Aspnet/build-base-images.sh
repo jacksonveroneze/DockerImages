68ff19fbc067032f905b829edb4040d54d06ea50
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-01-07 08:54:52 -0300

    Removido .net 6 on ubuntu.

--- Aspnet/build-base-images.sh
+++ Aspnet/build-base-images.sh
@@ -4,5 +4,4 @@
 docker build -f ./Alpine/Dockerfile-6-Alpine -t jacksonveroneze/aspnet:6.0-alpine ./Alpine/
 
 #Ubuntu
-docker build -f ./Ubuntu/Dockerfile-6-Ubuntu -t jacksonveroneze/aspnet:6.0-ubuntu ./Ubuntu/
 docker build -f ./Ubuntu/Dockerfile-7-Ubuntu -t jacksonveroneze/aspnet:7.0-ubuntu ./Ubuntu/
--- Aspnet/build-xray-images.sh
+++ Aspnet/build-xray-images.sh
@@ -4,5 +4,4 @@
 docker build -f ./Alpine/Dockerfile-6-Alpine-xray -t jacksonveroneze/aspnet:6.0-alpine-xray ./Alpine/
 
 #Ubuntu
-docker build -f ./Ubuntu/Dockerfile-6-Ubuntu-xray -t jacksonveroneze/aspnet:6.0-ubuntu-xray ./Ubuntu/
 docker build -f ./Ubuntu/Dockerfile-7-Ubuntu-xray -t jacksonveroneze/aspnet:7.0-ubuntu-xray ./Ubuntu/
