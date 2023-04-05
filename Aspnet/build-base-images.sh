a899e9898d7a12e93d066f3d683f829d6b1d2be0
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-04-05 07:28:56 -0300

    feat: ajustes

--- Aspnet/build-base-images.sh
+++ Aspnet/build-base-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Alpine
-docker build -f ./Alpine/Dockerfile-6-Alpine -t jacksonveroneze/aspnet:6.0-alpine ./Alpine/
+docker build -f ./Alpine/Dockerfile-6-Alpine -t jacksonveroneze/aspnet:7.0-alpine ./Alpine/
 
 #Ubuntu
 docker build -f ./Ubuntu/Dockerfile-7-Ubuntu -t jacksonveroneze/aspnet:7.0-ubuntu ./Ubuntu/
