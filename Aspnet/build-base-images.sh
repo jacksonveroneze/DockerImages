80dd3616e6d9794f4da79efebe3c5f35312dac0b
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-04-05 07:31:48 -0300

    feat: ajustes

--- Aspnet/build-base-images.sh
+++ Aspnet/build-base-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Alpine
-docker build -f ./Alpine/Dockerfile-6-Alpine -t jacksonveroneze/aspnet:7.0-alpine ./Alpine/
+docker build -f ./Alpine/Dockerfile-7-Alpine -t jacksonveroneze/aspnet:7.0-alpine ./Alpine/
 
 #Ubuntu
 docker build -f ./Ubuntu/Dockerfile-7-Ubuntu -t jacksonveroneze/aspnet:7.0-ubuntu ./Ubuntu/
--- Aspnet/build-xray-images.sh
+++ Aspnet/build-xray-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Alpine
-docker build -f ./Alpine/Dockerfile-6-Alpine-xray -t jacksonveroneze/aspnet:7.0-alpine-xray ./Alpine/
+docker build -f ./Alpine/Dockerfile-7-Alpine-xray -t jacksonveroneze/aspnet:7.0-alpine-xray ./Alpine/
 
 #Ubuntu
 docker build -f ./Ubuntu/Dockerfile-7-Ubuntu-xray -t jacksonveroneze/aspnet:7.0-ubuntu-xray ./Ubuntu/
