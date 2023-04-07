f4dc037a2fd9ebe045421d08929563abdb301f71
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-04-07 10:54:53 -0300

    feat: ajustes

--- Aspnet/build-xray-images.sh
+++ Aspnet/build-xray-images.sh
@@ -5,3 +5,6 @@ docker build -f ./Alpine/Dockerfile-7-Alpine-xray -t jacksonveroneze/aspnet:7.0-
 
 #Ubuntu
 docker build -f ./Ubuntu/Dockerfile-7-Ubuntu-xray -t jacksonveroneze/aspnet:7.0-ubuntu-xray ./Ubuntu/
+
+#Debian
+docker build -f ./Debian/Dockerfile-7-Debian-xray -t jacksonveroneze/aspnet:7.0-debian-xray ./Debian/
