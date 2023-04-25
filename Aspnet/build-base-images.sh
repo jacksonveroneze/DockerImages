c12dd5a49845f8bf3b28c855c53cac8561ec052d
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-04-25 11:52:04 -0300

    feat: ajustes

--- Aspnet/build-base-images.sh
+++ Aspnet/build-base-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Alpine
-docker build -f ./Alpine/Dockerfile-7-Alpine -t jacksonveroneze/aspnet:7.0-alpine .
+docker build -f ./Alpine/Dockerfile-7 -t jacksonveroneze/aspnet:7.0-alpine .
 
 #Ubuntu
-docker build -f ./Ubuntu/Dockerfile-7-Ubuntu -t jacksonveroneze/aspnet:7.0-ubuntu .
+docker build -f ./Ubuntu/Dockerfile-7 -t jacksonveroneze/aspnet:7.0-ubuntu .
 
 #Debian
-docker build -f ./Debian/Dockerfile-7-Debian -t jacksonveroneze/aspnet:7.0-debian .
+docker build -f ./Debian/Dockerfile-7 -t jacksonveroneze/aspnet:7.0-debian .
--- Aspnet/docker-entrypoint.sh
+++ Aspnet/docker-entrypoint.sh
@@ -5,5 +5,6 @@ OS=$(cat /etc/*-release | egrep "PRETTY_NAME" | cut -d = -f 2 | tr -d '"')
 echo "- System Operation: $OS"
 echo "- Run application: $APP_NAME"
 echo "- EnvironmentName: $ASPNETCORE_ENVIRONMENT"
+echo "- AspNetCoreUrls: $ASPNETCORE_URLS"
 echo ""
 dotnet $APP_NAME
